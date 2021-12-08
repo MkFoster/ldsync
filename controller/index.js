18c19377421e756bc07dd14913b3afd02aeb3d53
Mark Foster <mark@mkfoster.com>
2021-12-07 23:52:57 -0500

    Added some rudimentary authentication for the console.

--- controller/index.js
+++ controller/index.js
@@ -6,6 +6,12 @@ const fs = require("fs");
 const http = require("http");
 const ws = require("ws");
 const cors = require("cors");
+require("dotenv").config({ path: "variables.env" });
+
+//Todo: Replace authentication with a proper auth mechanism (I.e., Express w/Passport)
+var userList = {
+    ld: process.env.LD_PASS,
+};
 
 const app = express();
 
@@ -27,22 +33,55 @@ const server = http.createServer(
 
 const wss = new ws.WebSocketServer({ server });
 
+let heartbeat = false;
+
 wss.on("connection", (socket, req) => {
     //This server becomes a repeater
     socket.on("message", (msg) => {
-        console.log("msg: ", msg.toString());
-        socket.send(msg.toString());
-        wss.clients.forEach((client) => {
-            client.send(msg.toString());
-        });
+        const msgStr = msg.toString();
+        //console.log("msgStr: ", msgStr);
+        const msgObj = JSON.parse(msgStr);
+        if (msgObj.type == "auth") {
+            if (authenticate(msgObj.user, msgObj.password)) {
+                socket.authenticated = true;
+                socket.send(JSON.stringify({ type: "authOK" }));
+                console.log(msgObj.user + " has authenticated.");
+            } else {
+                socket.send(JSON.stringify({ type: "authFailed" }));
+                console.log(msgObj.user + " failed login.");
+            }
+        } else {
+            if (socket.authenticated) {
+                socket.authenticated = true;
+                wss.clients.forEach((client) => {
+                    if (
+                        client !== socket &&
+                        client.readyState === WebSocket.OPEN
+                    ) {
+                        client.send(msgStr);
+                    }
+                });
+            }
+        }
     });
 
-    const heartbeat = setInterval(() => {
-        wss.clients.forEach((client) => {
-            client.send(JSON.stringify({ heartbeat: "ping" }));
-        });
-    }, 3000);
+    if (!heartbeat) {
+        heartbeat = setInterval(() => {
+            wss.clients.forEach((client) => {
+                client.send(JSON.stringify({ heartbeat: "ping" }));
+            });
+        }, 3000);
+    }
+    console.log("Num Clients: ", wss.clients.size);
 });
+
+function authenticate(user, password) {
+    if (userList[user] && userList[user] == password) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
 server.listen(80, "localhost");
 server.on("listening", () =>
